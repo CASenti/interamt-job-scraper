d09ffbe534d047bb44257ad67e14f7d314a1fc2d
CASenti <180032508+CASenti@users.noreply.github.com>
2024-09-02 15:56:15 +0200

    Create __init__.py

---
+++ filter/__init__.py
@@ -0,0 +1,6 @@
+# filter/__init__.py
+# (Leave empty or initialize filter-level variables here)
+
+from .filter_processor import load_filter_list, apply_filter
+
+__all__ = ['load_filter_list', 'apply_filter']
